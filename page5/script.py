84d06d3e1f92e5a5839c306dc68edfc8ffbe5fa9
reskybayu234 <75589544+reskybayu234@users.noreply.github.com>
2022-04-12 14:42:43 +0700

    Python Progate Study 2

---
+++ page5/script.py
@@ -0,0 +1,10 @@
+# Assign a dictionary to the fruits variable
+fruits = {'apple' : 'manzana',
+          'orange' : 'naranja'
+}
+
+# Print the value with the key 'orange'
+print(fruits['orange'])
+
+# By using the fruits dictionary, print 'apple is ____ in Spanish'
+print(f"apple is {fruits['apple']} in Spanish")
---
+++ page7/script.py
@@ -0,0 +1,5 @@
+fruits = {'apple': 'manzana', 'orange': 'naranja', 'grape': 'uva'}
+
+# With a for loop, print '____ is ____ in Spanish'
+for value in fruits:
+  print(value + " is " + fruits[value] + " in Spanish") 
